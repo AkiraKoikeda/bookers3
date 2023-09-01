9faf23b42abc9a550a6598980af86daf4202d3d7
AkiraKoikeda <AkiraKoikedaAkiraKoikeda135214468+AkiraKoikeda@users.noreply.github.com>
2023-09-01 12:48:00 +0000

    top

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_for :admins
+
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  devise_for :admins, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  scope module: :public do
+    root to: 'homes#top'
+    get '/homes/about' => 'homes#about'
+    resources :users
+    resources :books
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
