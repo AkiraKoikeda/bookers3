7b8a6e704f20bc32be1c3449ef8ca0b045a084d6
AkiraKoikeda <AkiraKoikedaAkiraKoikeda135214468+AkiraKoikeda@users.noreply.github.com>
2023-09-02 00:27:03 +0000

    user_show

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,7 +1,17 @@
 class Public::UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
   end
 
+
   def edit
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :body)
   end
 end
