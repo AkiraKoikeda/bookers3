3d0e6ecfe7830d6fab6b4007e5045552c348a0c9
AkiraKoikeda <AkiraKoikedaAkiraKoikeda135214468+AkiraKoikeda@users.noreply.github.com>
2023-09-15 06:30:41 +0000

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_31_072228) do
+ActiveRecord::Schema.define(version: 2023_09_15_060712) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2023_08_31_072228) do
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "homes", force: :cascade do |t|
