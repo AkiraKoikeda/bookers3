d9fe52a39ce61659e6dc0d0304c016f988fc90ad
AkiraKoikeda <AkiraKoikedaAkiraKoikeda135214468+AkiraKoikeda@users.noreply.github.com>
2023-09-19 09:38:26 +0000

    image

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,11 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
 
-  def get_profile_image
-    unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg')
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg')
     end
-    profile_image.variant(resize_to_limit: [100, 100]).processed
+    image.variant(resize_to_limit: [width, height]).processed
   end
 end
